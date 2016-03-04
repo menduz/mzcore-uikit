edec2f77ef78842216ebb7b4bae01046523c545a
Agustin Mendez (Menduz) <lord.yo.wo@gmail.com>
2016-03-04 15:31:48 -0300

    bower

---
+++ index.ts
@@ -0,0 +1,8 @@
+﻿mz.loadCss(module.getPath('./uikit.css'));
+
+export * from './mz-sidebar';
+
+@MzClear.Template('<div style="clear:both" />')
+@MzClear.ConfigureUnwrapped
+@MzClear.RegisterComponent('clear')
+export class MzClear extends mz.Widget {}
