a2290ed0fc596ee508d97ab7afcae2bc9172160d
Agustin Mendez (Menduz) <lord.yo.wo@gmail.com>
2016-03-07 12:40:31 -0300

    added mz-tab & mz-taber

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 ﻿mz.loadCss(module.getPath('./uikit.css'));
 
 export * from './mz-sidebar';
+export * from './mz-autocomplete';
+export * from './mz-tabs';
 
 @MzClear.Template('<div style="clear:both" />')
 @MzClear.ConfigureUnwrapped
